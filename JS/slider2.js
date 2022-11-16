75c2e5b4935217a5e1928c3897e814fb6c196906
Marina Titova <marina-titova-1992@mail.ru>
2022-11-16 14:45:33 +0300

    slider2

---
+++ JS/slider2.js
@@ -0,0 +1,10 @@
+const swiper2 = new Swiper2(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
